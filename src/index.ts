458884b5c0b437bb597441433ee16aa2663de149
daemon <0xDAEF0F@proton.me>
2023-10-10 12:25:20 -0600

    edit

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import { unwrap, unwrapOr, map } from './primitives'
 import { unsafeDivision, safeDivision } from './functions'
-
 import assert from 'assert'
-import * as dotenv from 'dotenv'
-dotenv.config()
 
 async function main() {
-    // *WITHOUT RUST'S OPTION TYPE*
-    let doubledQuotient
+    // *WITHOUT RUST OPTION TYPE*
+    let doubledQuotient // You need to reassign it later.
     try {
         const myQuotient = unsafeDivision(42, 0)
-        // The same as `map` since we are transforming the value.
+
+        // The same as `map` since we are transforming the successful value.
         doubledQuotient = myQuotient * 2
     } catch (e: any) {
         assert(e.message === 'division by zero.')
+
         // The same as `unwrapOr` (we are handling the error)
         doubledQuotient = 69
     }
@@ -24,26 +23,29 @@ async function main() {
     const maybeQuotientA = safeDivision(42, 0)
 
     if (maybeQuotientA.kind === 'None') {
-        // this means division by zero.
-        // you can handle error now
-        // or do something else like reassigment.
+        // This means division by zero.
+        // You can handle the error gracefully now.
     }
 
     // EXAMPLE B
     //
-    // since division is possible, unwrapping as well.
+    // Since we know division is possible, you can `unwrap` safely.
     const maybeQuotientB = safeDivision(42, 1)
     const quotientB = unwrap(maybeQuotientB)
     assert(quotientB === 42)
 
     // EXAMPLE C
     //
-    // default value will be chosen because there is division by zero and thus an error.
+    // Default value will be chosen because there is division by zero — Error.
     const maybeQuotientC = safeDivision(42, 0)
     const quotientOrDefaultValueC = unwrapOr(maybeQuotientC, 69)
     assert(quotientOrDefaultValueC === 69)
 
     // EXAMPLE D
+    //
+    // Map will transform the `Some` value if there is one,
+    // if not it will still be `None`. We can unwrap in this example
+    // because we know it is *not* `None`.
     const maybeQuotientD = safeDivision(42, 1)
     const doubleIfPossible = map(maybeQuotientD, (x: number) => x * 2)
     const value = unwrap(doubleIfPossible)
